bcac775998d3c4c707b780fd9e5c7cde515736aa
francisco <gonzalezf@vcu.edu>
2017-10-05 00:21:20 -0400

    Comments added

--- glide ajax jawn JSON.js
+++ glide ajax jawn JSON.js
@@ -1,16 +1,19 @@
+//Server-side script include
 autoFill = Class.create();
 autoFill.prototype = Object.extendsObject(AbstractAjaxProcessor, {
 		fillValues: function () {
+			//init
 			var gr = new GlideRecord('TABLE_NAME_HERE');
 			var obj = {};
-
+			
+			//query for sys_id
 			gr.addQuery('sys_id', this.getParameter('sysparm_new'));
 			gr.query();
 			while (gr.next()) {
-
+				//add "assignment group or something" to obj
 				obj.group = String(gr.THING);
-
-				var data = JSON.stringify(obj);
+				//MAKE JSON and return string
+				var data = JSON.stringify(obj); //JSON formatted string
 				return data;
 			}
 		}
@@ -18,20 +21,23 @@ autoFill.prototype = Object.extendsObject(AbstractAjaxProcessor, {
 		type: 'autoFill'
 });
 
+
+
+//client script (On Change)
 function onChange(control, oldValue, newValue, isLoading, isTemplate) {
 	if (isLoading || newValue === '') {
 		return;
 	}
-
+	//script include info
 	var gAjax = new GlideAjax('autoFill');
 	gAjax.addParam('sysparm_name', 'fillValues');
-
+	//pull the new updated value
 	gAjax.addParam('sysparm_new', newValue);
 	gAjax.getXML(xmlResponse);
 	function xmlResponse(response) {
 		var answer = response.responseXML.documentElement.getAttribute("answer");
-		answer = JSON.parse(answer);
-		console.log(answer);
-		g_form.setValue('assignment_group', answer.group);
+		answer = JSON.parse(answer); //JSON string back to an object
+		console.log(answer); //log
+		g_form.setValue('assignment_group', answer.group); //set value from ajax
 	}
 }
